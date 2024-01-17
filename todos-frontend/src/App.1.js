8f02dd4fd2c13df5003d74c6484123cc5aca52bc
lindacom <linda@lindacom>
2024-01-17 19:55:26 +0000

    lint

---
+++ todos-frontend/src/App.1.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { NewTodoForm } from './NewTodoForm';
+import { TodoList } from './TodoList';
+
+export function App() {
+
+  const [todos, setTodos] = useState({});
+
+  useEffect(() => {
+    const loadTodos; ();
+  }, {});
+
+  const createTodo = todoText => {
+    // ...
+  };
+
+  const completeTodo = todoId => {
+    // ...
+  };
+
+  const deleteTodo = todoId => {
+    // ...
+  };
+
+
+  return (
+    <div>
+      <h1>My todos</h1>
+      <NewTodoForm onClickCreate=/>
+
+      <TodoList
+        todos=
+            onCompleteTodo=
+            onDeleteTodo=/>
+    </div>
+  );
+}
--- todos-frontend/src/App.js
+++ todos-frontend/src/App.js
@@ -1,42 +1,4 @@
 import './App.css';
-import {useState} from 'react';
-import {NewTodoForm} from './NewTodoForm';
-import { TodoList } from './TodoList';
-
-function App() {
-
-  const [todos, setTodos] = useState({});
-
-  useEffect (() => {
-    const loadTodos = async() => {
-      // ...
-          }
-    const loadTodos(); } {});
-
-    const createTodo = todoText => {
-      // ...
-          }
-
-    const completeTodo = todoId => {
-      // ...
-    }
-
-    const deleteTodo = todoId => {
-      // ...
-    }
-  
-
-  return (
-    <div>
-   <h1>My todos</h1>
-   <NewTodoForm onClickCreate = {} />
-
-   <TodoList
-   todos={}
-   onCompleteTodo = {}
-   onDeleteTodo = {} />
-   </div>
-  );
-}
+import { App } from './App.1';
 
 export default App;
